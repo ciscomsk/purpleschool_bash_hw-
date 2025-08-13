c6fb6b8e2ff05a709a966bc04083b7b2f2ae716a
ciscomsk <kuznetzoff@gmail.com>
2025-08-13 15:07:07 +0300

    hw2

--- hw2/7.sh
+++ hw2/7.sh
@@ -3,5 +3,5 @@ echo $current_day
 echo "Уже $current_day"
 read -p "Какое сегодня число?" current_date
 echo $current_date
-echo "Не думал что current_day будет $current_date числа"
+echo "Не думал что $current_day будет $current_date числа"
 echo "Я считаю что сегодня $(date)"
